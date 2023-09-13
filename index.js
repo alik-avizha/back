9a505df95013635daeed363c670cf3dde2403eec
Alik_Avizha <aleksandravizha@icloud.com>
2023-09-13 14:17:05 +0300

    fix: some

--- index.js
+++ index.js
@@ -43,10 +43,6 @@ io.on("connection", (socket) => {
 
 let port = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-    res.send("Server is running.");
-});
-
 server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
