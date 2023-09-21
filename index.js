e1369a2973daf66c7e62a5df129104d7da4d30b1
Alik_Avizha <aleksandravizha@icloud.com>
2023-09-22 00:23:03 +0300

    fix: cors

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ const http = require("http");
 const server = http.createServer(app);
 const cors = require("cors");
 
-const corsOptions = {
-    origin: ['http://localhost:3000', 'https://back-psi-brown.vercel.app'],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
     optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
-
-app.use(cors());
+    credentials: true,
+}));
 
 const socket = require("socket.io");
 const { Configuration, OpenAIApi } = require("openai");
